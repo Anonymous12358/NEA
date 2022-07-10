4714e40a227094d19f34852311a1b622abdd9f1a
Jem <jabennett@perse.co.uk>
2022-07-10 17:40:34 +0100

    Make match_line return indices into board

--- game/Board.py
+++ game/Board.py
@@ -1,59 +1,74 @@
 import itertools
+from collections.abc import Sequence
+from dataclasses import dataclass
+
 import numpy as np
-from typing import Tuple, List
 
 
 class Board:
-    def __init__(self, dimensions: Tuple[int, ...]):
+    @dataclass
+    class Line:
+        tiles: Sequence[int]
+        tile_indices: Sequence[tuple[int, ...]]
+        center: int
+
+    def __init__(self, dimensions: tuple[int, ...]):
         self.__data = np.zeros(dimensions, dtype='int8')
 
-    def get_node(self, coords: Tuple[int, ...]) -> int:
+    def get_node(self, coords: tuple[int, ...]) -> int:
         if len(coords) != self.__data.ndim:
             raise ValueError("Must provide a number of coordinates equal to the number of dimensions of the board")
         return self.__data[coords]
 
-    def remove(self, coords: Tuple[int, ...]):
+    def remove(self, coords: tuple[int, ...]):
         if len(coords) != self.__data.ndim:
             raise ValueError("Must provide a number of coordinates equal to the number of dimensions of the board")
         self.__data[coords] = 0
 
-    def place(self, coords: Tuple[int, ...], color: int):
+    def place(self, coords: tuple[int, ...], color: int):
         if len(coords) != self.__data.ndim:
             raise ValueError("Must provide a number of coordinates equal to the number of dimensions of the board")
         if color <= 0:
             raise ValueError(f"Invalid color {color} should be greater than 0")
         self.__data[coords] = color
 
-    def get_lines(self, coords: Tuple[int, ...]) -> List[Tuple[int, np.ndarray]]:
+    def get_lines(self, coords: tuple[int, ...]) -> list[Line]:
         """
         Get all lines, orthogonal or diagonal in any number of dimensions, through a given center
         :param coords: The coordinates of the center
         :returns: A list of tuples of the index of the center in each line, and the line itself
         """
-
+        
         if len(coords) != self.__data.ndim:
             raise ValueError("Must provide a number of coordinates equal to the number of dimensions of the board")
+
+        # Create an array of indexes so we can tell where the returned tiles came from
+        # The 0th dimension ranges over dimensions of the board
+        # Named iarray to aid the reader in resolving variable names
+        iarray = np.indices(self.__data.shape)
 
         result = []
 
         for directs_num in range(3 ** len(coords)):
             # The direction in which this line travels in each dimension
             # directs_num // 3**i % 3 extracts the ith digit of directs_num in ternary
-            directs = [directs_num // 3 ** i % 3 - 1 for i in range(len(coords))]
+            directs = tuple(directs_num // 3 ** i % 3 - 1 for i in range(len(coords)))
             # No line travels through 0 dimensions
             if all(direction == 0 for direction in directs):
                 continue
 
             # Transform the array so that the line travels forward in all dimensions in which it travels
-            transform_indices = []
+            # We perform operations on iarray and then use those to index the board in order to get the tiles
+            # Start with a slice(None) to skip the 0th dimension of iarray
+            transform_indices = [slice(None)]
             for ordinate, direction in zip(coords, directs):
                 if direction == -1:
                     transform_indices.append(slice(None, None, -1))
                 else:
                     transform_indices.append(slice(None))
-            transformed = self.__data[tuple(transform_indices)]
+            transformed_iarrray = iarray[tuple(transform_indices)]
 
-            # Transform coords so that they are coords into transformed
+            # Transform coords so that they are coords into the transformed array
             transformed_coords = tuple(
                 length-1 - ordinate if direction == -1 else ordinate
                 for length, ordinate, direction in zip(self.__data.shape, coords, directs)
@@ -70,7 +85,7 @@ class Board:
             # To achieve this, the center must have the same distance from the start in every dimension
             # It must also have the same distance from the end so that the array is the same length in every dimension
             # We also crop out the dimensions in which the line doesn't travel at this stage
-            crop_indices = []
+            crop_indices = [slice(None)]
             for ordinate, end_distance, direction in zip(transformed_coords, end_distances, directs):
                 if direction == 0:
                     # In this dimension, we only want the index that contains the center
@@ -86,11 +101,20 @@ class Board:
 
                     crop_indices.append(slice(start_index, end_index))
 
-            cropped = transformed[tuple(crop_indices)]
+            cropped_iarray = transformed_iarrray[tuple(crop_indices)]
 
+            # Take the diagonal from iarray for each dimension, and combine into a tuple
             # np.einsum can get a diagonal in an arbitrary number of dimensions
-            line = np.einsum(f'{"i"*cropped.ndim}->i', cropped)
-            # The returned line is a writeable view, so copy it so that operations on it can't alter the board
-            result.append((min_ordinate, np.copy(line)))
+            tile_indices = tuple(
+                np.einsum(f'{"i"*(cropped_iarray.ndim-1)}->i', dimension)
+                for dimension in cropped_iarray
+            )
+
+            # Make tiles a copy so that operations on it can't alter the board
+            tiles = np.copy(self.__data[tile_indices])
+
+            # Convert tile_indices from a tuple of arrays to an array of tuples, since the latter is more useful
+            # elsewhere in the program
+            result.append(Board.Line(tiles, list(zip(*tile_indices)), min_ordinate))
 
         return result
--- game/Pattern.py
+++ game/Pattern.py
@@ -1,8 +1,9 @@
 from collections.abc import Sequence
 from typing import Optional
 import re
-import numpy as np
 import string
+
+from game.Board import Board
 
 _pattern_validator = re.compile(r'''
     [#.0-9a-zA-Z-]*
@@ -26,39 +27,38 @@ class Pattern:
 
         self.__string = s.replace("[", "").replace("]", "")
 
-    def match_line(self, line_center: int, line: Sequence[int]) -> Optional[int]:
+    def match_line(self, line: Board.Line) -> Optional[Sequence[tuple[int, ...]]]:
         """
         Attempt to match this pattern to a given line and center
-        :param line_center: The position of the center within the line
         :param line: The line to match
-        :return: The first position at which the pattern matches, or None if it doesn't
+        :return: The positions at which the line matched, as indexes into the board
         """
         if self.__center is not None:
             # Skip the start of line to force the centers to line up
-            start = line_center - self.__center
-            if self._full_match(line[start:start + len(self.__string)]):
-                return start
+            start = line.center - self.__center
+            if self._full_match(line.tiles[start:start + len(self.__string)]):
+                return self._get_match_locations(line, start)
             return None
         else:
             # Try matching from every position that would include the line center
-            for start in range(max(0, line_center - len(self.__string) + 1),
-                               min(line_center + 1, len(self.__string) - len(line) - 1)):
-                match = self._full_match(line[start:start + len(self.__string)])
+            for start in range(max(0, line.center - len(self.__string) + 1),
+                               min(line.center + 1, len(line.tiles) - len(self.__string) + 1)):
+                match = self._full_match(line.tiles[start:start + len(self.__string)])
                 if match:
-                    return start
+                    return self._get_match_locations(line, start)
             return None
 
-    def _full_match(self, line: Sequence[int]) -> bool:
+    def _full_match(self, tiles: Sequence[int]) -> bool:
         """
         Get whether this pattern matches a given line exactly, ignoring centers
-        :param line: The line to match
+        :param tiles: The line to match
         :return: Whether or not this pattern matches the line exactly, ignoring centers
         """
-        if len(self.__string) != len(line):
+        if len(self.__string) != len(tiles):
             return False
 
         variables = {}
-        for char, tile in zip(self.__string, line):
+        for char, tile in zip(self.__string, tiles):
             if char == "#" and tile == 0:
                 return False
             elif char == "-" and tile != 0:
@@ -77,3 +77,6 @@ class Pattern:
                     variables[char] = tile
 
         return True
+
+    def _get_match_locations(self, line: Board.Line, start: int) -> Sequence[tuple[int, ...]]:
+        return line.tile_indices[start : start + len(self.__string)]
